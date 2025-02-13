e682b5e5c4e4939ce62cb2f563ceca9199e514de
MMXXII <perfilev621@gmail.com>
2025-02-13 13:01:12 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите предложение: ");
+        string input = Console.ReadLine().Replace(" ", ""); // Убираем пробелы
+
+        int count = 0;
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == input[i - 1])
+                count++;
+        }
+
+        Console.WriteLine($"Количество одинаковых соседних букв: {count}");
+    }
+}
